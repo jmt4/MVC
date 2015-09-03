d140858272bc90183350d75ba6caa2fe548865ea
jmt4 <jimmtriphahn@gmail.com>
2015-09-03 07:24:40 -0700

    First commit of demo project

---
+++ MVCDemo/MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            /* We can pass data to views with ViewBag object */
+            /* The .Countries is a dynamic variable. We can access it in the view */
+            /*  */
+            ViewData["Countries"] = new List<string>()
+            {
+                "India",
+                "US",
+                "UK",
+                "Canada",
+            };
+
+            return View();
+        }
+    }
+}
---
+++ MVCDemo/MVCDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        /* get and set are auto implemented properties */
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+    }
+}
